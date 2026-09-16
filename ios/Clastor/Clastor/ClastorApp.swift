a15882b71f7f735c5120e35831764d571ed269d4
Chethin Weerakkody <chethiche123@gmail.com>
2026-09-16 18:52:55 +1000

    mobile xcode project initialised

---
+++ ios/Clastor/Clastor/ClastorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClastorApp.swift
+//  Clastor
+//
+//  Created by Chethin Weerakkody on 16/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClastorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
